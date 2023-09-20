68bc72bb6839ed9d15289912e2870e78187cf349
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-09-20 07:10:44 -0700

    main-betty-handling

--- monty_sayed/main.c
+++ monty_sayed/main.c
@@ -3,34 +3,30 @@
 #include <string.h>
 #include "monty.h"
 
-unsigned int line_number = 1;
-
 int main(int argc, char *argv[])
 {
 	FILE *file;
 	char *line = NULL;
 	char *opcode;
- 
+	unsigned int line_number = 1;
+
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-
 	line = (char *)malloc(256);
 	if (line == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-
 	while (fgets(line, 256, file) != NULL)
 	{
 		opcode = strtok(line, " \t\n");
@@ -40,7 +36,6 @@ int main(int argc, char *argv[])
 		}
 		line_number++;
 	}
-
 	fclose(file);
 	if (line != NULL)
 	{
