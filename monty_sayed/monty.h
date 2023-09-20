e709bf4d9cc1b586fe6fdad3ca5aa6d9820c6d2e
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-09-20 07:14:16 -0700

    betty-monty.h

--- monty_sayed/monty.h
+++ monty_sayed/monty.h
@@ -7,20 +7,22 @@
 
 typedef struct stack_s
 {
-    int n;
-    struct stack_s *prev;
-    struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 typedef struct instruction_s
 {
-    char *opcode;
-    void (*f)(stack_t **stack);
+	char *opcode;
+	void (*f)(stack_t **stack);
 } instruction_t;
 
+/*Global variables*/
 extern stack_t *stack;
 extern unsigned int line_number;
 
+/*Functions prototypes*/
 void push(stack_t **stack);
 void pall(stack_t **stack);
 void pint(stack_t **stack);
