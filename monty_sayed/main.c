65757d83a8a72697d386ee9c3d9f0fa27fc4c9a1
Abdullah <abdallahmosbah25@gmail.com>
2023-09-20 07:02:12 -0700

    betty

--- monty_sayed/main.c
+++ monty_sayed/main.c
@@ -7,22 +7,22 @@ unsigned int line_number = 1;
 
 int main(int argc, char *argv[])
 {
-        FILE *file;
-        char *line = NULL;
+	FILE *file;
+	char *line = NULL;
 	char *opcode;
-    
-        if (argc != 2)
-        {
-                fprintf(stderr, "USAGE: monty file\n");
-                exit(EXIT_FAILURE);
-        }
+ 
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
-        file = fopen(argv[1], "r");
-        if (file == NULL)
-        {
-                fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-                exit(EXIT_FAILURE);
-        }
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
 	line = (char *)malloc(256);
 	if (line == NULL)
@@ -31,21 +31,20 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-        while (fgets(line, 256, file) != NULL)
-        {
-                opcode = strtok(line, " \t\n");
-                if (opcode != NULL && strcmp(opcode, "#") != 0)
-                {
-                        execute(opcode, &stack);
-                }
-                line_number++;
-        }
+	while (fgets(line, 256, file) != NULL)
+	{
+		opcode = strtok(line, " \t\n");
+		if (opcode != NULL && strcmp(opcode, "#") != 0)
+		{
+			execute(opcode, &stack);
+		}
+		line_number++;
+	}
 
-        fclose(file);
-        if (line != NULL)
-        {
-                free(line);
-        }
-
-        return (0);
+	fclose(file);
+	if (line != NULL)
+	{
+		free(line);
+	}
+	return (0);
 }
