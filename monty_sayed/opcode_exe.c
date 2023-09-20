7b8e5cfa3549bee1b3c3c8cbbee27d703f2da5b8
Abdullah <abdallahmosbah25@gmail.com>
2023-09-20 07:23:24 -0700

    opexe betty

--- monty_sayed/opcode_exe.c
+++ monty_sayed/opcode_exe.c
@@ -1,38 +1,42 @@
 #include "monty.h"
-
+/**
+ * execute - Execute Monty bytecode command
+ * @opcode: The Monty bytecode command to execute
+ * @stack: Pointer to the top of the stack
+ */
 void execute(char *opcode, stack_t **stack)
 {
-    if (strcmp(opcode, "push") == 0)
-    {
-        push(stack);
-    }
-    else if (strcmp(opcode, "pall") == 0)
-    {
-        pall(stack);
-    }
-    else if (strcmp(opcode, "pint") == 0)
-    {
-        pint(stack);
-    }
-    else if (strcmp(opcode, "pop") == 0)
-    {
-        pop(stack);
-    }
-    else if (strcmp(opcode, "swap") == 0)
-    {
-        swap(stack);
-    }
-    else if (strcmp(opcode, "add") == 0)
-    {
-        add(stack);
-    }
-    else if (strcmp(opcode, "nop") == 0)
-    {
-        nop(stack);
-    }
-    else
-    {
-        fprintf(stderr, "L%d: unknown instruction: %s\n", line_number, opcode);
-        exit(EXIT_FAILURE);
-    }
+	if (strcmp(opcode, "push") == 0)
+	{
+		push(stack);
+	}
+	else if (strcmp(opcode, "pall") == 0)
+	{
+		pall(stack);
+	}
+	else if (strcmp(opcode, "pint") == 0)
+	{
+		pint(stack);
+	}
+	else if (strcmp(opcode, "pop") == 0)
+	{
+		pop(stack);
+	}
+	else if (strcmp(opcode, "swap") == 0)
+	{
+		swap(stack);
+	}
+	else if (strcmp(opcode, "add") == 0)
+	{
+		add(stack);
+	}
+	else if (strcmp(opcode, "nop") == 0)
+	{
+		nop(stack);
+	}
+	else
+	{
+		fprintf(stderr, "L%d: unknown instruction: %s\n", line_number, opcode);
+		exit(EXIT_FAILURE);
+	}
 }
