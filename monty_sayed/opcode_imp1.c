b45ea07865b3dffd0102b9d1fa9c5d8d0d55b8e6
Abdullah <abdallahmosbah25@gmail.com>
2023-09-20 07:39:47 -0700

    betty opcode1

--- monty_sayed/opcode_imp1.c
+++ monty_sayed/opcode_imp1.c
@@ -2,9 +2,14 @@
 #include <stdlib.h>
 #include "monty.h"
 
-stack_t *stack = NULL; 
+stack_t *stack = NULL;
 
-void push(stack_t **stack) 
+/**
+ * push - Push an integer onto the stack
+ * @stack: Pointer to the top of the stack
+ */
+
+void push(stack_t **stack)
 {
 	int value;
 	stack_t *new_node;
@@ -17,75 +22,94 @@ void push(stack_t **stack)
 		exit(EXIT_FAILURE);
 	}
 
-	value = atoi(value_str);	
+	value = atoi(value_str);
 	new_node = malloc(sizeof(stack_t));
-    if (new_node == NULL) 
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-    new_node->n = value;
-    new_node->prev = NULL;
-    new_node->next = *stack;
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
 
-    if (*stack != NULL) 
-    {
-        (*stack)->prev = new_node;
-    }
+	if (*stack != NULL)
+	{
+		(*stack)->prev = new_node;
+	}
 
-    *stack = new_node;
+	*stack = new_node;
 }
 
-void pall(stack_t **stack) 
+/**
+ * pall - Print all elements of the stack
+ * @stack: Pointer to the top of the stack
+ */
+void pall(stack_t **stack)
 {
-    stack_t *current = *stack;
+	stack_t *current = *stack;
 
-    while (current != NULL) 
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
 }
 
-void pint(stack_t **stack) 
+/**
+ * pint - Print the top element of the stack
+ * @stack: Pointer to the top of the stack
+ */
+void pint(stack_t **stack)
 {
-    if (*stack == NULL) {
-        fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    printf("%d\n", (*stack)->n);
+	printf("%d\n", (*stack)->n);
 }
 
-void pop(stack_t **stack) 
+/**
+ * pop - Remove the top element of the stack
+ * @stack: Pointer to the top of the stack
+ */
+void pop(stack_t **stack)
 {
 	stack_t *temp;
-    if (*stack == NULL) 
-    {
-        fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-        exit(EXIT_FAILURE);
-    }
 
-    temp = *stack;
-    *stack = (*stack)->next;
-    if (*stack != NULL) 
-    {
-        (*stack)->prev = NULL;
-    }
-    free(temp);
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *stack;
+	*stack = (*stack)->next;
+	if (*stack != NULL)
+	{
+		(*stack)->prev = NULL;
+	}
+	free(temp);
 }
 
-void swap(stack_t **stack) 
+/**
+ * swap - Swap the top two elements of the stack
+ * @stack: Pointer to the top of the stack
+ */
+void swap(stack_t **stack)
 {
 	int temp;
-    if (*stack == NULL || (*stack)->next == NULL) 
-    {
-        fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
 
-    temp = (*stack)->n;
-    (*stack)->n = (*stack)->next->n;
-    (*stack)->next->n = temp;
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
 }
