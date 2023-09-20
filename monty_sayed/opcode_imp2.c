378c5b1ac43ac449f72a0616304a129915d40fe7
Abdullah <abdallahmosbah25@gmail.com>
2023-09-20 07:43:26 -0700

    try

--- monty_sayed/opcode_imp2.c
+++ monty_sayed/opcode_imp2.c
@@ -1,28 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "monty.h"
-
-void add(stack_t **stack) 
+/**
+ * add - Add the top two elements of the stack
+ * @stack: Pointer to the top of the stack
+ */
+void add(stack_t **stack)
 {
 	stack_t *temp;
 
-    if (*stack == NULL || (*stack)->next == NULL) 
-    {
-        fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    (*stack)->next->n += (*stack)->n;
+	(*stack)->next->n += (*stack)->n;
 
-    temp = *stack;
-    *stack = (*stack)->next;
-    if (*stack != NULL) 
-    {
-        (*stack)->prev = NULL;
-    }
-    free(temp);
+	temp = *stack;
+	*stack = (*stack)->next;
+	if (*stack != NULL)
+	{
+		(*stack)->prev = NULL;
+	}
+	free(temp);
 }
 
-void nop(stack_t **stack __attribute__((unused))) 
+/**
+ * nop - Do nothing (no operation)
+ * @stack: Pointer to the top of the stack (unused)
+ */
+void nop(stack_t **stack __attribute__((unused)))
 {
 }
