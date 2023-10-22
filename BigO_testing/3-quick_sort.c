74fea54b396849334facc21ca5bb5301b9c7c077
Abdullah <abdallahmosbah25@gmail.com>
2023-10-22 09:36:49 -0700

    quick

---
+++ BigO_testing/3-quick_sort.c
@@ -0,0 +1,78 @@
+#include "sort.h"
+
+int partition(int* array, size_t size, int left, int right);
+void recursive_sort(int* array, size_t size, int left, int right);
+void quick_sort(int* array, size_t size);
+
+/**
+ * partition - ordering a subset of an array.
+ * @array: an array of ints.
+ * @size: array size.
+ * @left: array first element.
+ * @right: array last element.
+ * Return: The pivot position.
+ */
+int partition(int* array, size_t size, int left, int right)
+{
+	int* pivot, i, j;
+
+	pivot = array + right;
+	for (i = j = left; j < right; j++)
+	{
+		if (array[j] < *pivot)
+		{
+			if (i < j)
+			{
+				swap(array + j, array + i);
+				print_array(array, size);
+			}
+			i++;
+		}
+	}
+
+	if (array[i] > *pivot)
+	{
+		swap(array + i, pivot);
+		print_array(array, size);
+	}
+
+	return (i);
+}
+
+/**
+ * recursive_sort - Implement the quicksort algorithm through recursion.
+ * @array: an array of ints.
+ * @size: array size.
+ * @left: array first element.
+ * @right: array last element.
+ *
+ * Description: Uses the Lomuto partition scheme.
+ */
+void recursive_sort(int* array, size_t size, int left, int right)
+{
+	int part;
+
+	if (right - left > 0)
+	{
+		part = partition(array, size, left, right);
+		recursive_sort(array, size, left, part - 1);
+		recursive_sort(array, size, part + 1, right);
+	}
+}
+
+/**
+ * quick_sort - Sort an array of integers in ascending
+ *              order using the quicksort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Uses the Lomuto partition scheme. Prints
+ *              the array after each swap of two elements.
+ */
+void quick_sort(int* array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	recursive_sort(array, size, 0, size - 1);
+}
