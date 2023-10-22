567629bd5cb9f1cb9672e0496a8fe6933887f7d6
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-10-22 11:39:26 +0200

    task 4 advanced

---
+++ BigO_testing/100-shell_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * shell_sort - function that uses Shell sorting algorithm
+ * to sort integers in ascending order using Knuth sequence
+ * @array: array to be sorted
+ * @size: array size
+ */
+void shell_sort(int *array, size_t size)
+{
+    size_t gap = 1;
+    size_t i, j;
+    int tmp;
+
+    while (gap < size / 3)
+        gap = gap * 3 + 1;
+    while (gap > 0)
+    {
+        for (i = gap; i < size; i++)
+        {
+            tmp = array[i];
+            j = i;
+            while (j >= gap && array[j - gap] > tmp)
+            {
+                array[j] = array[j - gap];
+                j -= gap;
+            }
+            array[j] = tmp;
+        }
+        gap = (gap - 1) / 3;
+        print_array(array, size);
+    }
+}
