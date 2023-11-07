2ebacbf1f1b1ca4c08bab59b8dd1307c9c673242
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-11-07 18:58:53 +0100

    file storage update

--- AirBnB_clone/models/engine/file_storage.py
+++ AirBnB_clone/models/engine/file_storage.py
@@ -2,6 +2,8 @@
 """ define the file storage class """
 
 import json
+import os
+import models
 from models.base_model import BaseModel
 from models.review import Review
 from models.state import State
@@ -12,16 +14,38 @@ from models.amenity import Amenity
 
 class FileStorage:
     """ the file storage main class """
-    __file_path = ""
-    __objects = dict
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
         """ returns a dictionary """
-        return self.__objects
+        return FileStorage.__objects
     
-    def new(self):
+    def new(self, obj):
         """ set an obj in __objects """
-        keydict = "{}.{}".format(obj.__class__.__name__,
-                obj.id)
-        self.__objects[keydict] = obj
+        dictkey = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[dictkey] = obj
+    
+    def save(self):
+        """ serialize __objects to json file """
+        serialized = {}
+        for dictkey, obj in FileStorage.__objects.items():
+            serialized[dictkey] = obj.to_dict()
+        with open(FileStorage.__file_path, 'w') as file:
+            json.dump(serialized, file)
 
+    def reload(self):
+        """ deserialize json file to __objects """
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, 'r') as file:
+                try:
+                    serialized = json.load(file)
+                    for dictkey, obj_dict in serialized.items():
+                        class_name = obj_dict.pop("__class__", None)
+                        if class_name:
+                            obj_id = dictkey.split('.')[1]
+                            cls = models.classes[class_name]
+                            obj = cls(**obj_dict)
+                            FileStorage.__objects[dictkey] = obj
+                except FileNotFoundError:
+                    pass
