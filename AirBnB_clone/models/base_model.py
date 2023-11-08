3dd155c292587ff4671923b04710e430f8a971b5
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-11-08 18:55:01 +0100

    update tests

--- AirBnB_clone/models/base_model.py
+++ AirBnB_clone/models/base_model.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
 import models
-import datetime
-import uuid
+from datetime import datetime
+from uuid import uuid4
 
 class BaseModel:
     
     def __init__(self, *args, **kwargs):
-        self.updated_at = datetime.datetime.now()
+        """self.updated_at = datetime.datetime.now()
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         if len(kwargs) == 0:
+            models.storage.new(self)"""
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, time_format)
+                else:
+                    self.__dict__[k] = v
+        else:
             models.storage.new(self)
 
 
@@ -18,12 +30,12 @@ class BaseModel:
         return new
        
     def save(self):
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.today()
         models.storage.save() 
 
     def to_dict(self):
-        self.__dict__.update({'__class__': self.__class__.__name__})
-        for key in self.__dict__:
-            if key == 'updated_at' or key == 'created_at':
-                self.__dict__[key] = datetime.datetime.now().isoformat()
-        return self.__dict__
+        new_dict = self.__dict__.copy()
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        new_dict["__class__"] = self.__class__.__name__
+        return new_dict
