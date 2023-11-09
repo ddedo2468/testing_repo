fabe5dd1957f469d8b5988e335895f5f1a0779d9
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-11-09 17:41:20 +0100

    show and create

--- AirBnB_clone/console.py
+++ AirBnB_clone/console.py
@@ -2,6 +2,7 @@
 """ the main hbnb console """
 import cmd
 from models import *
+from models.engine.file_storage import FileStorage
 import os
 
 class HBNBCommand(cmd.Cmd):
@@ -23,5 +24,32 @@ class HBNBCommand(cmd.Cmd):
         """ handle an empty line prints nothing """
         pass
     
+    def do_create(self, arg):
+        """"""
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.validCls:
+            print("** class doesn't exist **")
+        else:
+            obj = BaseModel()
+            obj.save()
+            print(obj.id)
+    
+    def do_show(self, arg):
+        """"""
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.validCls:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif args[1] not in FileStorage.__objects:
+            print("** no instance found **")
+        else:
+            obj = FileStorage.__objects.get(args[0], args[1])
+            print(obj)
+
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
