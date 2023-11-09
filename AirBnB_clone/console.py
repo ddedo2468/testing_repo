040f72c9364bbf320388e1508c7f4497e560929d
Abdullah <abdallahmosbah25@gmail.com>
2023-11-09 09:14:33 -0800

    args and import

--- AirBnB_clone/console.py
+++ AirBnB_clone/console.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """ the main hbnb console """
 import cmd
-from models import *
-from models.engine.file_storage import FileStorage
 import os
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     """ define the hbnb command interpreter """
@@ -39,17 +45,18 @@ class HBNBCommand(cmd.Cmd):
     def do_show(self, arg):
         """"""
         args = arg.split()
+        all_obj = storage.all()
+
         if len(args) == 0:
             print("** class name missing **")
         elif args[0] not in HBNBCommand.validCls:
             print("** class doesn't exist **")
         elif len(args) == 1:
             print("** instance id missing **")
-        elif args[1] not in FileStorage.__objects:
+        elif args[1] not in all_obj:
             print("** no instance found **")
         else:
-            obj = FileStorage.__objects.get(args[0], args[1])
-            print(obj)
+            print(all_obj["{}.{}".format(args[0], args[1])])
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
