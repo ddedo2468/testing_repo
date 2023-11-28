51592bcf9dc47e6827790a342bb15659b692c1f0
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-11-28 08:47:47 -0800

    betty

--- binary_trees/9-binary_tree_height.c
+++ binary_trees/9-binary_tree_height.c
@@ -7,24 +7,24 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_h, right_h;
+	size_t left_h, right_h;
 
-    if (tree == NULL)
-        return 0;
+	if (tree == NULL)
+		return (0);
 
-    left_h = binary_tree_height(tree->left);
-    right_h = binary_tree_height(tree->right);
+	left_h = binary_tree_height(tree->left);
+	right_h = binary_tree_height(tree->right);
 
-    if (left_h > right_h)
-    {
-        if (tree->left == NULL && tree->right == NULL)
-            return left_h;
-        return (left_h + 1);
-    }
-    else
-    {
-        if (tree->left == NULL && tree->right == NULL)
-            return right_h;
-        return (right_h + 1);
-    }
+	if (left_h > right_h)
+	{
+		if (tree->left == NULL && tree->right == NULL)
+			return (left_h);
+		return (left_h + 1);
+	}
+	else
+	{
+		if (tree->left == NULL && tree->right == NULL)
+			return (right_h);
+		return (right_h + 1);
+	}
 }
