bfeb70667404d64ee1737bb6ffd4acb73aff176e
ddedo2468 <58207352+ddedo2468@users.noreply.github.com>
2023-12-18 16:34:42 +0200

    last edit

--- AirBnBV2/models/base_model.py
+++ AirBnBV2/models/base_model.py
@@ -29,6 +29,10 @@ class BaseModel:
                                                          '%Y-%m-%dT%H:%M:%S.%f')
             else:
                 kwargs['created_at'] = datetime.now()
+            
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+
 
             self.__dict__.update(kwargs)
 
