ad43b2ca0aeba75abc4582a5d272967b5c65f7c2
ddedo2468 <58207352+ddedo2468@users.noreply.github.com>
2023-12-18 18:52:29 +0200

    sqldd

--- AirBnBV2/setup_mysql_test.sql
+++ AirBnBV2/setup_mysql_test.sql
@@ -1,5 +1,4 @@
---prepares a MySQL server (setup test)
-
+-- prepares a MySQL server (setup test)
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
