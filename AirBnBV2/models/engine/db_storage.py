2601fa5900ed817d04d9a9c1acb71dc81fcdb11a
ddedo2468 <58207352+ddedo2468@users.noreply.github.com>
2023-12-19 16:44:52 +0200

    try smth.

--- AirBnBV2/models/engine/db_storage.py
+++ AirBnBV2/models/engine/db_storage.py
@@ -20,14 +20,19 @@ class DBStorage:
     __session = None
 
     def __init__(self):
-        """Instatntiates a new model"""
-        self.__engine = create_engine(
-            'mysql+mysqldb://{}:{}@{}/{}'.format(getenv('HBNB_MYSQL_USER'),
-                                                 getenv('HBNB_MYSQL_PWD'),
-                                                 getenv('HBNB_MYSQL_HOST'),
-                                                 getenv('HBNB_MYSQL_DB')),
-            pool_pre_ping=True)
-        if getenv('HBNB_ENV') == 'test':
+        """ constructor method """
+        env = getenv("HBNB_ENV")
+        user = getenv("HBNB_MYSQL_USER")
+        passwd = getenv("HBNB_MYSQL_PWD")
+        db = getenv("HBNB_MYSQL_HOST ")
+        host = getenv("HBNB_MYSQL_DB")
+
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user, passwd, host, db),
+                                      pool_pre_ping=True)
+
+        
+        if env == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
