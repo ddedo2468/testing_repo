fdc4dd36f254bd0c502079528daef14b6644e65f
ddedo2468 <58207352+ddedo2468@users.noreply.github.com>
2023-12-19 16:23:44 +0200

    commitedss

--- AirBnBV2/models/base_model.py
+++ AirBnBV2/models/base_model.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from sqlalchemy import Column, String, Integer, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base
+Base = declarative_base()
 
 
 class BaseModel:
